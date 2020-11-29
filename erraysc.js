3c92c20c44c4da90730f42c0c74121a69fab3e3d
Vi <99063304@mydavinci.nl>
2020-11-29 22:48:16 +0100

    Array

--- erraysc.js
+++ erraysc.js
@@ -1,9 +1,12 @@
-var tafels document.getElementById('tafels');
+var tafels = document.getElementById('tafels');
+var arrayEen = [2,4,6,8];
 
 function tafel1() {
-  for (var i = 0; i <10; i++) {
-  var total3 = arrayEen[i] * arrayTwee[i];
-  vermeen.innerHTML = vermeen.innerHTML + arrayEen[i] + 'x'+ arrayTwee[i] + '=' + total3 + '<br>'
+  for (var i = 0; i < arrayEen.length; i++) {
+    for (var a = 1; a <=10; a++) {
+      var total3 = [a] * arrayEen[i];
+      tafels.innerHTML = tafels.innerHTML + [a] + 'x'+ arrayEen[i] + '=' + total3 + '<br>'
 }
 }
-tafel();
+}
+tafel1();
---
+++ erraysd.js
@@ -0,0 +1,9 @@
+
+
+
+function spel() {
+var spellen = ['Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo'];
+var randomspellen = spellen[Math.floor(spellen.length*Math.random())];
+document.write('<h2>'randomspellen);
+}
+spel();
